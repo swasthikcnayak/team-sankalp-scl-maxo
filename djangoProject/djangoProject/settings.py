6dd9da1bca234a42d8092083f5bd84a2e28ec5de
swasthik <swasthik0304@gmail.com>
2020-12-08 00:34:32 +0530

    updated settings and urls of djangoproject

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'academics.apps.AcademicsConfig',
+    'assignments.apps.AssignmentsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

543b076f78f70f1142b0d7c5829e816e69440ef9
swasthik <swasthik0304@gmail.com>
2020-12-07 22:52:03 +0530

    updated the assignment model

--- project/assignments/models.py
+++ project/assignments/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-# from academics.models import Subject, Class
+from academics.models import Subject, Class
 from users.models import StudentProfile, TeacherProfile
 
 
 class Assignment(models.Model):
     teacher = models.ForeignKey(TeacherProfile, on_delete=models.SET_NULL, verbose_name="teacher", null=True)
-    # Class = models.ForeignKey(Class, on_delete=models.SET_NULL, verbose_name='class', null=True)
-    # subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name='subject')
+    Class = models.ForeignKey(Class, on_delete=models.SET_NULL, verbose_name='class', null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name='subject')
     assignment_name = models.CharField(max_length=20)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
@@ -17,21 +17,21 @@ class Assignment(models.Model):
     def clean(self):
         self.assignment_name = self.assignment_name.upper()
 
-    '''def __str__(self):
-        return self.assignment_name + "+" + self.Class + "+" + self.subject'''
+    def __str__(self):
+        return self.assignment_name + "+" + self.Class + "+" + self.subject
 
     class Meta:
         unique_together = ('subject', 'assignment_name')
 
 
 class Submission(models.Model):
-    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, verbose_name='assignments')
+    assignments = models.ForeignKey(Assignment, on_delete=models.CASCADE, verbose_name='assignments')
     student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, verbose_name='student')
     answer = models.URLField()
     time_submitted = models.DateTimeField()
 
-    '''def __str__(self):
-        return self.assignments + "+" + self.student'''
+    def __str__(self):
+        return self.assignments + "+" + self.student
 
     class Meta:
         unique_together = ('assignments', 'student')

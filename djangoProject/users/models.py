113eab81e52c8c5d760c17b283489ec96d44f3d7
swasthik <swasthik0304@gmail.com>
2020-12-10 17:56:08 +0530

    Updated folder structure with attendance and marks app for easy modifications.

--- djangoProject/users/models.py
+++ djangoProject/users/models.py
@@ -112,25 +112,4 @@ class Teach(models.Model):
         unique_together = ('Class', 'subject', 'teacher')
 
 
-class Mark(models.Model):
-    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, verbose_name="student")
-    teacher = models.ForeignKey(TeacherProfile, on_delete=models.SET_NULL, verbose_name="teacher", null=True)
-    exam_name = models.CharField(max_length=20)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name="subject")
-    marks_maximum = models.CharField(max_length=3)
-    marks_obtained = models.CharField(max_length=3)
 
-    class Meta:
-        unique_together = ('student', 'subject', 'exam_name')
-
-
-class Attendance(models.Model):
-    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, verbose_name="student")
-    teacher = models.ForeignKey(TeacherProfile, on_delete=models.SET_NULL, verbose_name="teacher", null=True)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name="subject")
-    classes_conducted = models.CharField(max_length=3)
-    classes_attended = models.CharField(max_length=3)
-    percentage = models.CharField(max_length=4)
-
-    class Meta:
-        unique_together = ('student', 'subject')

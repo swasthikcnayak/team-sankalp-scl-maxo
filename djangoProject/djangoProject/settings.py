888b4ea0bb118e28be10686a3d5cc5dce2f60149
swasthik <swasthik0304@gmail.com>
2020-12-10 14:48:56 +0530

    updated marks, attendance admin, assignments admin with search and view options.

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -134,6 +134,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'YOUR EMAIL ID HERE'
-EMAIL_HOST_PASSWORD = 'YOUR EMAIL PASSWORD HERE'
+EMAIL_HOST_USER = 'swasth69k@gmail.com'
+EMAIL_HOST_PASSWORD = 'swasthik2001'
 
--- djangoProject/users/admin.py
+++ djangoProject/users/admin.py
@@ -15,18 +15,32 @@ class UserAdminClass(UserAdmin):
 
 class StudentProfileAdminClass(admin.ModelAdmin):
     list_display = ('user', 'department', 'semester', 'section', 'cgpa')
-    search_fields = ('user__username', 'user__email','department__department_short_form',
+    search_fields = ('user__username', 'user__email', 'department__department_short_form',
                      'department__department_name', 'user__first_name', 'user__last_name', 'section', 'cgpa')
 
 
 class TeacherProfileAdminClass(admin.ModelAdmin):
     list_display = ('user', 'department', 'join_date')
     search_fields = ('user__username', 'user__email', 'user__first_name', 'user__lastname',
-                     'department__department_short_form','department__department_name')
+                     'department__department_short_form', 'department__department_name')
+
+
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('student', 'teacher', 'subject', 'classes_attended', 'classes_conducted', 'percentage')
+    search_fields = ('student__user__username', 'student__user__email', 'student__user__first_name',
+                     'student__user__last_name', 'subject__department__department_short_form',
+                     'subject__department__department_name','teacher__user__username','teacher__user__first_name')
+
+
+class MarksAdmin(admin.ModelAdmin):
+    list_display = ('student', 'subject', 'teacher', 'marks_obtained', 'marks_maximum')
+    search_fields = ('student__user__username', 'student__user__email', 'student__user__first_name',
+                     'student__user__last_name', 'subject__subject_name','subject__subject_short_form',
+                     'subject__department__department_short_form', 'subject__department__department_name')
 
 
 admin.site.register(User, UserAdminClass)
 admin.site.register(StudentProfile, StudentProfileAdminClass)
 admin.site.register(TeacherProfile, TeacherProfileAdminClass)
-admin.site.register(Attendance)
-admin.site.register(Mark)
+admin.site.register(Attendance, AttendanceAdmin)
+admin.site.register(Mark, MarksAdmin)

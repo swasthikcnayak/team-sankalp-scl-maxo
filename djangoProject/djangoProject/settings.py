52e62c2c14b30fb68060d5d18d40f7fe0edd5f33
swasthik <swasthik0304@gmail.com>
2020-12-13 12:23:20 +0530

    hiding credentials.

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'swasth69k@gmail.com'
-EMAIL_HOST_PASSWORD = 'swasthik2001'
+EMAIL_HOST_USER = 'YOUR EMAIL HERE'
+EMAIL_HOST_PASSWORD = 'YOUR EMAIL PASSWORD HERE'

3b560f773ef4b08f0beed805d7673d2d416ba0ec
swasthik <swasthik0304@gmail.com>
2020-12-22 14:40:24 +0530

    made use of mailgun and a domain name for easy sending and recieving mails.

--- djangoProject/attendance/models.py
+++ djangoProject/attendance/models.py
@@ -6,12 +6,11 @@ from users.models import StudentProfile, TeacherProfile
 
 class Attendance(models.Model):
     student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, verbose_name="student")
-    teacher = models.ForeignKey(TeacherProfile, on_delete=models.SET_NULL, verbose_name="teacher", null=True)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name="subject")
-    Class = models.ForeignKey(Class,on_delete=models.CASCADE,null=True)
-    classes_conducted = models.IntegerField()
-    classes_attended = models.IntegerField()
-    percentage = models.IntegerField()
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, verbose_name="subject",null=True)
+    Class = models.ForeignKey(Class,on_delete=models.SET_NULL,null=True)
+    classes_conducted = models.IntegerField(default=0)
+    classes_attended = models.IntegerField(default=0)
+    percentage = models.IntegerField(default=0)
 
     class Meta:
         unique_together = ('student', 'subject')
--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -135,8 +135,8 @@ LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'EMAIL ID HERE'
-EMAIL_HOST_PASSWORD = 'EMAIL PASSWORD HERE'
+EMAIL_HOST_USER = 'webapp@sankalp.live'
+EMAIL_HOST_PASSWORD = '13f9612e7045d6415f6d693099648bc6-b6190e87-bf1359a0'

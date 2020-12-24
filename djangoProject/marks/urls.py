4efd3778a3cb5d562897ecbfccd53938bb52fc3c
swasthik <swasthik0304@gmail.com>
2020-12-24 23:08:57 +0530

    completed marks module for teacher and student.

--- djangoProject/marks/urls.py
+++ djangoProject/marks/urls.py
@@ -3,5 +3,7 @@ import marks.views as marks_view
 
 urlpatterns = [
    path('', marks_view.view_list, name="view-list"),
-   #path('/<int:subjectId>', marks_view.view_subject_marks, name="view-subject-marks"),
+   path('<int:subjectId>/', marks_view.marks_detail, name="view_marks_detail"),
+   path('<int:subjectId>/<int:classId>',marks_view.students_list,name="view_marks_list_student"),
+   path('<int:subjectId>/<int:classId>/<int:studentId>',marks_view.student_marks_detail,name="view_marks_detail_student"),
 ]

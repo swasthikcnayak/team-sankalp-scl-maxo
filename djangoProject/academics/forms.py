cc987f88a4eb0721ae4c8c7c5eb3c6c746a0ef98
swasthik <swasthik0304@gmail.com>
2021-01-01 15:56:53 +0530

    git commit temp

--- djangoProject/academics/forms.py
+++ djangoProject/academics/forms.py
@@ -5,4 +5,9 @@ from academics.models import Note
 class AddNotesForm(forms.ModelForm):
     class Meta:
         model = Note
-        fields = ['chapter_number', 'chapter_name','link']
+        fields = ['chapter_number', 'chapter_name', 'link']
+
+        widgets = {
+            'chapter_number': forms.TextInput(attrs={'placeholder': 'Chapter number'}),
+            'chapter_name': forms.TextInput(attrs={'placeholder': 'Chapter Name'}),
+        }
--- djangoProject/academics/urls.py
+++ djangoProject/academics/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('notes/', academics_view.view_list, name="view-notes-subject-list"),
     path('notes/<int:subjectId>', academics_view.view_notes, name="view-notes"),
     path('class/', academics_view.class_list, name="view-class-list"),
-    path('class/<int:classId>',academics_view.view_class,name="view-class"),
+    path('class/<int:classId>', academics_view.view_class, name="view-class"),
 ]

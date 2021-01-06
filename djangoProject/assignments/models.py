7eed3711132544b2afb2c26b6825e83bb041d34b
swasthik <swasthik0304@gmail.com>
2021-01-06 15:17:08 +0530

    adding the breadcrumbs.

--- djangoProject/assignments/models.py
+++ djangoProject/assignments/models.py
@@ -1,6 +1,4 @@
-from django.core.validators import FileExtensionValidator
 from django.db import models
-## ,validators=[FileExtensionValidator(allowed_extensions=['*'])]
 from academics.models import Subject, Class
 from users.models import StudentProfile, TeacherProfile
 

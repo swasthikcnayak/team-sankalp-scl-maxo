948effa372c49d5988d269ed1048d548b24d9bde
swasthik <swasthik0304@gmail.com>
2021-01-09 19:25:47 +0530

    color for table

--- djangoProject/djangoProject/forms.py
+++ djangoProject/djangoProject/forms.py
@@ -5,17 +5,9 @@ ROLE_CHOICES = [
     ('THR', 'TEACHER'),
 ]
 
+
 class IssueForm(forms.Form):
-    id = forms.CharField(max_length=50, label='Registration number/ employee id')
-    email = forms.EmailField(label='Enter Email address')
+    id = forms.CharField(max_length=50, label='Registration number/ employee id',widget=forms.TextInput(attrs={'placeholder': 'Reg Number/Employee Id'}))
+    email = forms.EmailField(label='Enter Email address',widget=forms.EmailInput(attrs={'placeholder': 'Enter Email address'}))
     role = forms.ChoiceField(choices=ROLE_CHOICES, label='Select your role')
-    description = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 40}))
-
-    class Meta:
-        widgets = {
-            'id': forms.TextInput(attrs={'placeholder': 'Reg Number/Employee Id'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Enter Email address'}),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super(IssueForm, self).__init__(*args, **kwargs)
+    description = forms.CharField(widget=forms.Textarea(attrs={"rows": 10, "cols": 40,"placeholder":"Explain your issue, so that it is easy for us to help you"}))

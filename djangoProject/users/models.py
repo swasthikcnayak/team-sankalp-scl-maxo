3e3a4e395bfdbf6085b51655e0b8a8222179dea6
swasthik <swasthik0304@gmail.com>
2021-01-12 22:42:40 +0530

    updated requirements.txt

--- djangoProject/users/models.py
+++ djangoProject/users/models.py
@@ -77,23 +77,6 @@ class User(AbstractUser):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-    #
-    #     def save(self, *args, **kwargs):
-    #         super().save(*args, **kwargs)
-    #
-    #         img_read = storage.open(self.image.name, 'r')
-    #         img = Image.open(img_read)
-    #
-    #         if img.height > 300 or img.width > 300:
-    #             output_size = (300, 300)
-    #             img.thumbnail(output_size)
-    #             in_mem_file = io.BytesIO()
-    #             img.save(in_mem_file, format='JPEG')
-    #             img_write = storage.open(self.image.name, 'w+')
-    #             img_write.write(in_mem_file.getvalue())
-    #             img_write.close()
-    #
-    #         img_read.close()
 
 
 class StudentProfile(models.Model):
